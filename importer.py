49d435ac1fe47b7733832c0b2bf3e7d745ce81f0
Zach Pace <zpace@astro.wisc.edu>
2021-02-10 10:48:10 -0700

    add software version to config, incorporate into results basedir

--- importer.py
+++ importer.py
@@ -3,10 +3,12 @@ import faulthandler; faulthandler.enable()
 
 mpl_v = 'MPL-11'
 daptype = 'SPX-MILESHC-MASTARSSP'
+software_version = '1.1.0'
 csp_basedir = os.environ['STELLARMASS_PCA_CSPBASE']
-manga_results_basedir = os.environ['STELLARMASS_PCA_RESULTSDIR']
+manga_results_basedir = os.path.join(
+    os.environ['STELLARMASS_PCA_RESULTSDIR'], software_version)
 mocks_results_basedir = os.path.join(
-    os.environ['STELLARMASS_PCA_RESULTSDIR'], 'mocks')
+    os.environ['STELLARMASS_PCA_RESULTSDIR'], software_version, 'mocks')
 
 from astropy.cosmology import WMAP9
 cosmo = WMAP9
